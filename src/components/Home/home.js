efec1bd3d313975e01fd593ce2216771603facc2
recognizeyourself <recognizeyourself909@gmail.com>
2022-11-12 18:05:03 +0530

    merging banner and footer

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,17 +3,15 @@ import { Button } from "@mui/material";
 import Footer from "../Footer/Footer";
 import { Typography } from "@mui/material";
 
+import { UniversalFlexDiv } from "../TypographyAssets/TypographyAssets";
+
 import Banner from "./banner";
 function Home() {
   return (
-    <>
+    <UniversalFlexDiv flexDirection={"column"} alignItems={"inherit"}>
+      <Banner />
       <Footer></Footer>
-      <div className="">
-        <Banner />
-
-        <div className="Left-Dashboard"></div>
-      </div>
-    </>
+    </UniversalFlexDiv>
   );
 }
 
