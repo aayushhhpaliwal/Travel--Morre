da7dbbd08696a3ae625e80ade39fd78359e8f159
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2022-11-14 18:47:48 +0530

    layout changes

--- src/components/PlaceTravelCard/PlacestravelCard.js
+++ src/components/PlaceTravelCard/PlacestravelCard.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import Dubai from '../../assets/Places/Dubai.jpg'
+
+
+
 import { ComponentContainer, ImageContainer, SubHeader, UniversalFlexDiv, } from '../TypographyAssets/TypographyAssets'
+import data from '../../assets/Data/data'
+import { Title } from '@mui/icons-material'
 
-function PlacestravelCard(props) {
-  return (
+function PlacestravelCard(
+  {title, description, index, img}
+) {
+    return (
     <>
-   
-    <ImageContainer src={Dubai} width="22rem" height="30rem" borderRadius="1rem"   marginLeft="5%" position="absolute">
+  
+    <ImageContainer src={img && img} width="15rem" height="23rem" borderRadius="1rem"   marginLeft="5%" position="absolute">
     </ImageContainer>
-
-    <ComponentContainer borderRadius={"1rem"} width="22rem" height="20rem" marginTop="4rem" paddingTop="27rem" padding="0rem 2rem">
-
-    <h1>Dubai</h1>
-
+    <ComponentContainer borderRadius={"1rem"} width="13rem" height="17rem" marginTop="4rem" paddingTop="20rem" padding="0rem 2rem">
+    <h1>{title && title}</h1>
     <br></br>
     <hr width="80%"></hr>
     <SubHeader>
-    Discover Dubai, with world-class tourism and magnificent tailored destinations. Explore a wide range of choices and start planning your trip ...
-    </SubHeader>
+    { description && description}</SubHeader>
     </ComponentContainer>
    
     
