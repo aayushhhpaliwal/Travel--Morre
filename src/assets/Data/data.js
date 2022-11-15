5a028d21c55cd1c9dc15b33f22eaea532602dc43
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2022-11-15 13:22:46 +0530

    slider final 1

--- src/assets/Data/data.js
+++ src/assets/Data/data.js
@@ -1,56 +1,56 @@
 const data = [
     {
         title :"Dubai", 
-        desc : "Check 1", 
+        desc : "Dubai is the most populous city in the United Arab Emirates (UAE) and the capital of the Emirate of Dubai. Established in the 18th century as a small ...", 
         img : "Places/Dubai.jpg",
     
     },
     {
         title :"Maldives", 
-        desc : "Check 1",
+        desc : "The Maldives is a nation of islands in the Indian Ocean, that spans across the equator. The country is comprised of 1192 islands that stretch along a length o",
         img : "Places/Maldives.jpg",
     
     },
     {
         title :"Newyork", 
-        desc : "Check 3", 
+        desc : "New York City is a global cultural, financial, and media center with a significant influence on commerce, health care and life sciences, entertainment, research ...", 
         img : "Places/Newyork.jpg",
     
     },
     {
         title :"Japan", 
-        desc : "Check 3", 
+        desc : "Japan is an island country in East Asia. It is situated in the northwest Pacific Ocean, and is bordered on the west by the Sea of Japan, while extending ...", 
         img : "Places/Japan.jpg",
         
     },
 
-    {
-        title :"Malasia", 
-        desc : "Check 3", 
-        img : "Places/Malasia.jpg",
-        
-    },
-    {
+        {
         title :"Singapore", 
-        desc : "Check 3", 
+        desc : "Singapore, officially the Republic of Singapore, is a sovereign island country and city-state in maritime Southeast Asia...", 
         img : "Places/Singapore.jpg",
         
     },
     {
         title :"Thailand", 
-        desc : "Check 3", 
+        desc : "Thailand historically known as Siam and officially the Kingdom of Thailand, is a country in Southeast Asia, located at the centre of the Indochinese ...", 
         img : "Places/Bangok.jpg",
         
     },
     {
-        title :"Thailand", 
-        desc : "Check 3", 
+        title :"Malasia", 
+        desc : "Malaysia is a country in Southeast Asia. The federal constitutional monarchy consists of thirteen states and three federal territories, separated by the ...", 
+        img : "Places/Malasia.jpg",
+        
+    },
+    {
+        title :"Vietnam", 
+        desc : "Explore Vietnam's natural wonders, from epic waterfalls to the world's largest caves, with travel guides and nature-inspired trip ideas from Vietnam Tourism ...", 
         img : "Places/Japan.jpg",
         
     },
     {
         title :"Mauritius", 
-        desc : "Check 3", 
+        desc : "Mauritius, an Indian Ocean island nation, is known for its beaches, lagoons and reefs. The mountainous interior includes Black River Gorges...", 
         img : "Places/Mauritius.jpg",
         
     },
